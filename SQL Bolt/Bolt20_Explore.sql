fe4233e2b2a9808254cb94d3414c2b3365a3bb45
Matheus Machado de Almeida <40668494+MatheusMachado57@users.noreply.github.com>
2022-08-23 18:56:48 -0300

    att

--- SQL Bolt/Bolt20_Explore.sql
+++ SQL Bolt/Bolt20_Explore.sql
@@ -7,11 +7,8 @@ select * from north_american_cities where City like '%R%';
 select * from north_american_cities where Population between 3000000 and 9000000;
 select * from north_american_cities where Country in ('Canada');
 
-select * 
-from north_american_cities
-where Population > 
-      (select avg(Population) 
-	   from north_american_cities);
+select * from north_american_cities
+where Population > (select avg(Population) from north_american_cities);
 
 select City, Population, Population/(SELECT SUM(Population) 
 FROM north_american_cities)*100 as '%Pop' from north_american_cities;
@@ -21,6 +18,13 @@ alter table north_american_cities add column porc double;
 select sum(Population) from north_american_cities;
 update north_american_cities set porc = 38688645;
 update north_american_cities set porc = Population/porc;
+# update north_american_cities set porc = (Population/(SELECT SUM(Population) FROM north_american_cities)*100);
 
 select * from employees where ucase(Role) = 'Manager';
 select lcase(Role) from employees where lcase(Role) = 'Manager';
+
+select * from buildings where building_name in (select building from employees);
+select * from buildings where building_name in (select building from employees where Capacity > 20);
+
+select * from employees E, buildings B where B.Building_name = E.Building;
+select B.Building_name, E.Building from employees E, buildings B where B.Building_name = E.Building;
